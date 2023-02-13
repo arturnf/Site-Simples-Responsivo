3a3693b2c9cbff5994873cbd14017d6cb64179b8
Artur Felipe <arthur.f.assis09@gmail.com>
2023-02-13 18:55:10 -0300

    primeiro commit

---
+++ script/script.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector('.menu-hamburguer');
+const navAndroid = document.querySelector('.menu-android');
+
+function openMenu(){
+    navAndroid.classList.toggle('open');
+}
+
+
